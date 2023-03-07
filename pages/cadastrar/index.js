c5f960000c6e5cf41220772c46b4db50e1038bc9
Victor Beccare <vpdr1912@gmail.com>
2023-03-06 22:54:37 -0300

    Feat: creating sigunp page

---
+++ pages/cadastrar/index.js
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div>
+      <h1>cadastrar</h1>
+    </div>
+  );
+};
+
+export default Login;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
-export default () => (
-	<main>
-		<h1>Welcome to Next.js</h1>
-	</main>
-)
+import { Box, Button, Flex, Heading, Input } from "@chakra-ui/react";
+
+const Login = () => {
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        direction="column"
+        bgColor="gray.600"
+        px={32}
+				py={16}
+        borderRadius={8}
+				maxW="900px"
+      >
+        <Heading color="teal.400" textAlign="center" mb={8}>
+          The Next
+        </Heading>
+        <Input placeholder="email@email.com" variant="filled" mb={4} />
+        <Input placeholder="*******" variant="filled" mb={12}  />
+        <Button colorScheme="teal" mb={2}>
+          Entrar
+        </Button>
+        <Button variant="ghost" colorScheme="teal" mb={6}>
+          Cadastrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Login;
