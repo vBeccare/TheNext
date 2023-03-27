9427f08d1e928a7fa01de0297feab606489b525b
brunaramosv <brunaramos_v@hotmail.com>
2023-03-26 22:56:07 -0300

    Avaliação de cliente

--- pages/visualizar-produto/index.js
+++ pages/visualizar-produto/index.js
@@ -2,6 +2,8 @@ import { Button, Flex, Image, Text } from "@chakra-ui/react"
 import Header from "../../components/Header";
 
 const VisualizarProduto = () => {
+    const stars = document.querySelectorAll('.star');
+
     return (
         <Flex
             height="100vh"
@@ -25,10 +27,22 @@ const VisualizarProduto = () => {
                         iPhone 14 Pro Max. Câmera grande-angular de 48 MP para capturar detalhes impressionantes.
                         Dynamic Island, uma nova forma de interação no iPhone. Tela Sempre Ativa. E Detecção de
                         Acidente*, um novo recurso de segurança que liga para a emergência se você não puder.
-                       
+
                     </Text>
+                <div class="rating">
+                        <input type="radio" id="star5" name="rating" value="5" />
+                        <label for="star5" title="5 estrelas">&#9733;</label>
+                        <input type="radio" id="star4" name="rating" value="4" />
+                        <label for="star4" title="4 estrelas">&#9733;</label>
+                        <input type="radio" id="star3" name="rating" value="3" />
+                        <label for="star3" title="3 estrelas">&#9733;</label>
+                        <input type="radio" id="star2" name="rating" value="2" />
+                        <label for="star2" title="2 estrelas">&#9733;</label>
+                        <input type="radio" id="star1" name="rating" value="1" />
+                        <label for="star1" title="1 estrela">&#9733;</label>
+                    </div>
                     <Text marginTop="26px" maxWidth="1200px" fontSize={25}>
-                    R$15.499,00
+                        R$15.499,00
                     </Text>
                     <Button marginTop="200px" colorScheme="teal" mb={2} >
                         Comprar
